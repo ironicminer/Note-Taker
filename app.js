3b32adb070d9683201c0e01337a412a2d7c962e0
nate <rotarygurter@gmail.com>
2020-12-18 23:41:47 -0600

    update

--- app.js
+++ app.js
@@ -34,45 +34,44 @@ app.get("/api/notes", function (request, response) {
 
 app.post("/api/notes", (request, response) => {
   fs.readFile("./db/db.json", (err, data) => {
-      let noteData = data;
+    let noteData = data;
     if (err) throw err;
-  
-  noteData.push(request.body);
-  for (let i = 0; i < noteData.length; i++) {
-    noteData[i].id = i++;
+
+    noteData.push(request.body);
+    for (let i = 0; i < noteData.length; i++) {
+      noteData[i].id = i++;
+    }
+
+    fs.writeFile(db),
+      JSON.stringify(noteData),
+      (err) => {
+        if (err) throw err;
+        response.send(db);
+      };
+  });
+
+  //Start server
+  app.listen(PORT, function () {
+    console.log("Server started on port: " + PORT);
+  });
+
+  async function initialize() {
+    await readNotes();
+    setId();
   }
 
-  fs.writeFile(db),
-    JSON.stringify(noteData),
-    (err) => {
-      if (err) throw err;
-      response.send(db);
-    };
-
-});
-
-//Start server
-app.listen(PORT, function () {
-  console.log("Server started on port: " + PORT);
-});
-
-async function initialize() {
-  await readNotes();
-  setId();
-}
-
-async function readNotes() {
-  note = JSON.parse(await readFile(path.join(__dirname, "/db/db.json")));
-}
-
-function setId() {
-  let highID = 0;
-  if (note.length > 0) {
-    note.forEach(function (note) {
-      if (note.id > highID) highID = note.id;
-    });
+  async function readNotes() {
+    note = JSON.parse(await readFile(path.join(__dirname, "/db/db.json")));
   }
-  highID++;
-  id = highID;
-}
-//Stop Server
+
+  function setId() {
+    let highID = 0;
+    if (note.length > 0) {
+      note.forEach(function (note) {
+        if (note.id > highID) highID = note.id;
+      });
+    }
+    highID++;
+    id = highID;
+  }
+});
