4248b842899c55b70f1b8afacbbc0f7a68ec2be5
nate <rotarygurter@gmail.com>
2020-12-18 21:47:43 -0600

    oops

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const util = require("util");
 const path = require("path");
-const e = require("express");
+
 const readFile = util.promisify(fs.readFile);
 
 let app = express();
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //html routes
-app.get("/notes", function (request, response) {
+app.get("./notes", function (request, response) {
   response.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 //API routes
