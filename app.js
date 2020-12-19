68bcbda0f6452df063073598eebe19523e59149a
nate <rotarygurter@gmail.com>
2020-12-18 22:10:02 -0600

    fixing routing

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 //html routes
 app.get("/notes", function (request, response) {
-  response.sendFile(path.join(__dirname, "../public/notes.html"));
+  response.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 app.get("*", function (request, response) {
